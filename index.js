224995f13fab4fa6ef43ac182e7c687625f3c656
literallymya <36018938+literallymya@users.noreply.github.com>
2019-04-02 22:46:13 -0400

    drum kit part 1

---
+++ index.js
@@ -0,0 +1,68 @@
+// gives each drum an event listener
+
+var numberOfDrumButtons = document.querySelectorAll(".drum").length;
+
+for (var i = 0; i < numberOfDrumButtons; i++) {
+
+  document.querySelectorAll(".drum")[i].addEventListener("click", handleClick);
+}
+
+// when you click, drum sounds!
+
+function handleClick() {
+
+  var buttonInnerHTML = this.innerHTML;
+
+  switch (buttonInnerHTML) {
+    case "w":
+      var tom1 = new Audio('sounds/tom-1.mp3');
+      tom1.play();
+      break;
+
+    case "a":
+      var tom2 = new Audio('sounds/tom-2.mp3');
+      tom2.play();
+      break;
+
+    case "s":
+      var tom3 = new Audio('sounds/tom-3.mp3');
+      tom3.play();
+      break;
+
+    case "d":
+      var tom4 = new Audio('sounds/tom-4.mp3');
+      tom4.play();
+      break;
+
+    case "j":
+      var kickBass = new Audio('sounds/kick-bass.mp3');
+      kickBass.play();
+      break;
+
+    case "k":
+      var snare = new Audio('sounds/snare.mp3');
+      snare.play();
+      break;
+
+    case "l":
+      var crash = new Audio('sounds/crash.mp3');
+      crash.play();
+      break;
+
+
+    default: console.log(buttonInnerHTML);
+
+  }
+}
+
+//
+
+function HouseKeeper(yearsOfExp, name, cleaningRep) {
+  this.yearsOfExp = yearsOfExp;
+  this.name = name;
+  this.cleaningRep = cleaningRep;
+}
+
+var houseKeeper1 = new HouseKeeper(12, "Tom", ["lobby", "bedroom"]);
+
+console.log(houseKeeper1.name);
